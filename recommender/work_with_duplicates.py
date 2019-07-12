74fc618ff3afde78259a85e0daa0d46a8bd29964
al766 <al766@cornell.edu>
2019-07-12 11:31:15 -0400

    fixed weird groupby

--- recommender/work_with_duplicates.py
+++ recommender/work_with_duplicates.py
@@ -17,8 +17,8 @@ tfidf = TfidfVectorizer(stop_words='english')
 # features
 features = ['Name','Major','Class 1','Class 2','Class 3','Class 4','Interest 1','Interest 2','Interest 3','Hometown','Hometype']
 primary = 'Name'
-# groupby = 'Major'
-groupby = None
+groupby = 'Major'
+# groupby = None
 weights = {'Name': 0, 'Major': 30, 'Class 1': 20, 'Class 2': 20, 'Class 3': 20, 'Class 4': 20, 'Interest 1': 12, 'Interest 2': 12, 'Interest 3': 12, 'Hometown': 18, 'Hometype': 0}
 num = 2
 csv = 'Test Classes Extended.csv'
@@ -26,23 +26,20 @@ csv = 'Test Classes Extended.csv'
 
 # minimize number of global variables
 def convert_csv_to_matrix(csv, num):
-    # Load Employees Metadata
+    # Load data from csv
     metadata = pd.read_csv(csv)
     m0 = metadata[features]
     m0 = m0.reset_index()
     group_dict = {}
-    matches = []
+    matches = pd.DataFrame(columns=features + ['index'])
     ones = []
 
     def func_pairs(group):
+        # apply clean_df function to features
         m1 = group.copy()
         m1 = ci.clean_df(m1, features, primary)
         
         # BEGINNING ------------------------------------------------------------
-        
-        # Apply clean_data function to your features and create soup
-        # print(group)
-
         m1 = m1.assign(score = [''] * len(m1))
         for feature in features:
             if feature in weights:
@@ -56,28 +53,25 @@ def convert_csv_to_matrix(csv, num):
 
         # Compute the cosine similarity matrix
         cosine_sim = linear_kernel(tfidf_matrix, tfidf_matrix)
-
         # END -----------------------------------------------------------------
         
         # cosine_sim = ms.construct_similarity(m1)
-
         #Construct a reverse map of indices and employee names
-        # indices = pd.Series(m1.index, index=group[primary]).drop_duplicates()
         indices = pd.Series(group.index, index=group['index']).drop_duplicates()
+
         return get_pairs(group['index'].sample(frac=1), indices, cosine_sim, group, num)
-        # return get_pairs(group[primary].sample(frac=1), indices, cosine_sim, group, num)
 
     if groupby is not None:
         courses = m0[groupby].unique() # list of all unique department names
         
         for course in courses:
-            group = (m0[m0[groupby] == course]).reindex()
+            group = (m0[m0[groupby] == course]).reset_index().drop('level_0', axis=1)
             # keep track of groups with only one member
-            
+
             if len(group) == 1:
                 ones.append(group)
             else:
-                matches = matches + func_pairs(group)
+                matches = pd.concat([matches, func_pairs(group)], sort=False)
         
         if len(ones) == 1:
             for match in matches:
@@ -90,11 +84,11 @@ def convert_csv_to_matrix(csv, num):
             if len(ones) > 0:
                 matches[0].append(ones.pop(0))
         else:
-            df = pd.DataFrame(columns=features)
+            df = pd.DataFrame(columns=features + ['index'])
             for one in ones:
                 df = df.append(one, sort=False)
-            df = df.reindex()
-            matches = matches + func_pairs(df)
+            df = df.reset_index().drop('level_0', axis=1)
+            matches = pd.concat([matches, func_pairs(df)], sort=False)
     else:
         matches = func_pairs(m0)
     
@@ -114,10 +108,9 @@ def get_recommendations(name, indices, cosine_sim, list_to_remove, m0):
     # Get the employee indices
     emp_indices = []
     emp_sims = []
-    num = 0
     for i in sim_scores:
         if (len(emp_indices) == 1): break
-        if i[0] not in list_to_remove and i[0] != name:
+        if i[0] not in list_to_remove and i[0] != idx:
             emp_indices.append(i[0])
             emp_sims.append(i[1])
 
@@ -142,7 +135,7 @@ def get_random(mylist, num): # num = number of people per group
 def get_pairs(emplist, indices, cosine_sim, m0, num):
     # pairs = []
     list_to_remove = []
-    pairs = pd.DataFrame(columns=features)
+    pairs = pd.DataFrame(columns=features + ['index'])
     for e in emplist:
         if indices[e] not in list_to_remove:
             partner = list(get_random(get_recommendations(e, indices, cosine_sim, list_to_remove, m0), num)['index'])
@@ -163,7 +156,6 @@ def get_pairs(emplist, indices, cosine_sim, m0, num):
             pairs = pd.concat([pairs, extra], sort=False)
             pairs.append(pair)
             list_to_remove.sort(reverse=True)
- 
     return pairs
 
 # print(convert_csv_to_matrix(csv, num))
